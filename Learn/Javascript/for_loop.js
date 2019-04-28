8363311bba2bee307c671a9876fdbdb8a0c68c80
Max Venables <mv.maxvenables@gmail.com>
2019-04-28 14:20:32 +0100

    created files demonstrating for loops and while loops

---
+++ Learn/Javascript/for_loop.js
@@ -0,0 +1,9 @@
+//with numbers
+for (var i = 0; i < 10; i ++){ //I changes based on condition
+  console.log(i)
+}
+//with an array
+var array = ["A", "B", "C"]
+for (var x in array){ //iterates through positions of array from 0 to n
+  console.log(array[x])
+}
---
+++ Learn/Javascript/while_loop.js
@@ -0,0 +1,5 @@
+var x = 0;
+while(x < 10){ //iterates while a condition is true
+  console.log(x);
+  x++;
+}
